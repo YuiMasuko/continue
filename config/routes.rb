f790350a0769b6b8c895c510ca3f2b1ab1bfe7cd
yui masuko <yui.t.tom@gmail.com>
2021-09-09 23:30:56 +0900

    ルーティング設定 #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :groups
+  resources :groups do
+    resources :assigns, only: %w(create destroy)
+  end
   devise_for :users
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
