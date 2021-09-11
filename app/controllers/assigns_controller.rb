53c7f54bee2889bef996f0574a1f030670e102fe
yui masuko <yui.t.tom@gmail.com>
2021-09-11 11:23:14 +0900

    招待機能実装

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -3,7 +3,13 @@ class AssignsController < ApplicationController
 
   def create
     group = Group.find(params[:group_id])
-    user = User.find_by(assign_params)
+    user = User.find_by(specification: params[:specification])
+    if user && user.birthday.strftime('%Y-%m-%d') == params[:birthday]
+      group.invite_member(user)
+      redirect_to group_path(group), notice: '新メンバーを招待しました！'
+    else
+      redirect_to group_path(group), notice: 'ユーザーが見つかりませんでした'
+    end
 
   end
 
@@ -11,10 +17,7 @@ class AssignsController < ApplicationController
 
   end
 
-  private
-  def assign_params
-    params[:specification, :birthday]
-  end
+
 
 
 end
