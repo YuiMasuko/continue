1c0d89903f0f2507b3b5ba7fb00818264376b53f
yui masuko <yui.t.tom@gmail.com>
2021-09-12 12:03:00 +0900

    バリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :content, length: { maximum: 100 }
+  validates :image, presence: true
+  validates :event_on, presence: true
+
   belongs_to :user
   belongs_to :group
 end
