bb88a1410f4364487a6200dac5652e0a72270ce8
yui masuko <yui.t.tom@gmail.com>
2021-09-12 11:49:14 +0900

    NotNull追加設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_022210) do
+ActiveRecord::Schema.define(version: 2021_09_12_024407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 2021_09_12_022210) do
     t.date "event_on", null: false
     t.text "content"
     t.string "image", null: false
-    t.bigint "user_id"
-    t.bigint "group_id"
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_posts_on_group_id"
