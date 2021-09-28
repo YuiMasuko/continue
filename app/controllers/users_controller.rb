87f51d4cd3ab52a427c380002c1d2fd95af3984e
yui masuko <yui.t.tom@gmail.com>
2021-09-28 17:13:39 +0900

    recover before_action in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
-#  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show]
 
   def show
-    # if current_user == @user
-     @user = current_user
+    if current_user == @user
       @groups = current_user.groups
-    #else
-     # redirect_to user_path(current_user.id), notice: '他の人のマイページは見られません！'
-    #end
+    else
+      redirect_to user_path(current_user.id), notice: '他の人のマイページは見られません！'
+    end
   end
 
   private
