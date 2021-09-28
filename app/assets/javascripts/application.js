4b256d0980a86f44e6060ff602d399ff9796591a
yui masuko <yui.t.tom@gmail.com>
2021-09-28 18:09:51 +0900

    libraries requirement switched

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-// require turbolinks
 //= require jquery/dist/jquery.js
 //= require popper.js/dist/popper.js
 //= require bootstrap/dist/js/bootstrap.js
+//= require activestorage
+//= require turbolinks
 //= require_tree .
