f014e3c3d61a4a91fdc0b4d225c2e035b2108fca
yui masuko <yui.t.tom@gmail.com>
2021-09-28 18:37:45 +0900

    jquery requirement moved  onto top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery/dist/jquery.js
+//= require rails-ujs
 //= require popper.js/dist/popper.js
 //= require bootstrap/dist/js/bootstrap.js
 //= require activestorage
