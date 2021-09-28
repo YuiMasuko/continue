f7188b7a0097770fa4ffdd4cc7adb1dd91f8beda
yui masuko <yui.t.tom@gmail.com>
2021-09-28 16:59:42 +0900

    before_action removed from users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
-  before_action :set_user, only: [:show]
+#  before_action :set_user, only: [:show]
 
   def show
-    if current_user == @user
+    # if current_user == @user
+     @user = current_user
       @groups = current_user.groups
-    else
-      redirect_to user_path(current_user.id), notice: '他の人のマイページは見られません！'
-    end
+    #else
+     # redirect_to user_path(current_user.id), notice: '他の人のマイページは見られません！'
+    #end
   end
 
   private
