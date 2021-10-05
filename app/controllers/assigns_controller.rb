7f060d71a22dbb1dc934f0b8d873e1f0cc42835b
yui masuko <yui.t.tom@gmail.com>
2021-10-05 23:41:51 +0900

    invite birthday params change

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -5,6 +5,7 @@ class AssignsController < ApplicationController
     #招待機能
     group = Group.find(params[:group_id])
     user = User.find_by(specification: params[:specification])
+    binding.pry
     if user && user.birthday.strftime('%Y-%m-%d') == params[:birthday]
       group.invite_member(user)
       redirect_to group_path(group), notice: '新メンバーを招待しました！'
